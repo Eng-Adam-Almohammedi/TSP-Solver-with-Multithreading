8499366da55cdad7682f80789a11c00294d29c3c
Eng-Adam-Almohammedi <97549744+Eng-Adam-Almohammedi@users.noreply.github.com>
2024-02-01 17:56:55 +0300

    Add files via upload

---
+++ src/Point.java
@@ -0,0 +1,13 @@
+
+public class Point {
+
+    String name;
+    int x_c;
+    int y_c;
+
+    public Point(String name, int x_c, int y_c) {
+        this.name = name;
+        this.x_c = x_c;
+        this.y_c = y_c;
+    }
+}
